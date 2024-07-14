d705563bcbc2bc170f38b0c098ac3b23d11a3848
GorLena <zhura1977@list.ru>
2024-07-14 18:53:45 +0300

    README.md

--- src/test/java/org/example/hwjzmaven2/service/VacationServiceTest.java
+++ src/test/java/org/example/hwjzmaven2/service/VacationServiceTest.java
@@ -7,29 +7,36 @@ import org.junit.jupiter.api.Test;
 public class VacationServiceTest {
 
     @Test
-    /**/ public void shouldCalcExact() {
-
+     public void shouldCalcExpect() {
         VacationService service = new VacationService();
+        // подготавливаем данные:
         int income = 10_000;
         int expenses = 3_000;
         int threshold = 20_000;
         int expected = 3;
 
+         //вызываем целевой метод:
+        int actual = service.calculate(income,expenses,threshold);
 
-        Assertions.assertEquals(income, expenses, threshold);
+        // проводим проверку( сравниваем ожидаемый и фактический р-т):
+        Assertions.assertEquals( expected, actual);
     }
 
 
     @Test
     public void shouldCalcPact() {
         VacationService service = new VacationService();
+
+       // подшотавливаем данные:
         int income = 100_000;
         int expenses = 60_000;
         int threshold = 150_000;
         int expected = 2;
 
+        //вызываем целевой метод:
+        int actual = service.calculate( income,expenses,threshold);
 
-        Assertions.assertEquals(income, expenses, threshold);
+        Assertions.assertEquals( expected, actual);
     }
 }
 
