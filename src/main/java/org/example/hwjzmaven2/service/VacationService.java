81f76d7a0de03fe5c57632dbb873dd622c177878
GorLena <zhura1977@list.ru>
2024-07-14 10:35:34 +0300

    README.md

---
+++ src/main/java/org/example/hwjzmaven2/service/VacationService.java
@@ -0,0 +1,21 @@
+package org.example.hwjzmaven2.service;
+
+public class VacationService {
+
+    public int calculate(int income, int expenses) {
+        int count = 0; // счётчик месяцев отдыха
+        int money = 0; // количество денег на счету
+        for (int month = 0; month < 12; month++) {
+            if (money >= expenses) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses;
+            } else {
+                money = money + income;
+            }
+        }
+        return count;
+    }
+
+
+}
+
